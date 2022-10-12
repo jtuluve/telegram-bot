fe4b73af81c4cc83c9baac696dfe674403e238c6
jtuluve <74163310+jtuluve@users.noreply.github.com>
2022-10-12 18:27:15 +0530

    Update and rename bot.ts to bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -6,3 +6,7 @@ bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
